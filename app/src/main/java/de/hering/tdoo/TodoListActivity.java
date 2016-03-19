974a769605f9425c3b88f050167bdc99935e91a6
Frederik Bader <heringsfilet@gmail.com>
2016-03-19 16:27:28 +0100

    initial commit

---
+++ app/src/main/java/de/hering/tdoo/TodoListActivity.java
@@ -0,0 +1,136 @@
+package de.hering.tdoo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import de.hering.tdoo.model.ToDo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An activity representing a list of Todos. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link TodoDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class TodoListActivity extends AppCompatActivity {
+    private ListView listViews;
+    private ArrayList<ToDo> toDoItems;
+    private ArrayAdapter<ToDo> toDoItemsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        View recyclerView = findViewById(R.id.todo_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(ToDo.ITEMS));
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<ToDo.ToDoItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<ToDo.ToDoItem> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.todo_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).name);
+            holder.mContentView.setText(mValues.get(position).description);
+            if(mValues.get(position).isFavourite){
+                holder.mStarView.setVisibility(View.VISIBLE);
+            }else{
+                holder.mStarView.setVisibility(View.INVISIBLE);
+            }
+            holder.mCheckboxView.setChecked(mValues.get(position).isDone);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, TodoDetailActivity.class);
+                    intent.putExtra(TodoDetailFragment.ARG_ITEM_ID, holder.mItem.name);
+
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public final ImageView mStarView;
+            public final CheckBox mCheckboxView;
+            public ToDo.ToDoItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.name);
+                mContentView = (TextView) view.findViewById(R.id.dueDate);
+                mStarView = (ImageView) view.findViewById(R.id.starView);
+                mCheckboxView = (CheckBox) view.findViewById(R.id.checkBox);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+}
---
+++ app/src/main/java/de/hering/tdoo/model/ToDo.java
@@ -0,0 +1,60 @@
+package de.hering.tdoo.model;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ToDo {
+
+    public static final List<ToDoItem> ITEMS = new ArrayList<ToDoItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, ToDoItem> ITEM_MAP = new HashMap<String, ToDoItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    private static void addItem(ToDoItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.name, item);
+    }
+
+    private static ToDoItem createDummyItem(int position) {
+        ToDoItem tmp = new ToDoItem("ToDo " + position);
+        long offset = Timestamp.valueOf("2016-05-01 00:00:00").getTime();
+        long end = Timestamp.valueOf("2016-12-30 00:00:00").getTime();
+        long diff = end - offset + 1;
+        tmp.dueDate = new Date(offset + (long)(Math.random() * diff));
+        tmp.isFavourite = Math.random() < 0.3;
+        tmp.isDone = Math.random() < 0.5;
+        return tmp;
+    }
+
+    public static class ToDoItem {
+        public String name;
+        public String description;
+        public Boolean isDone = false;
+        public Boolean isFavourite = false;
+        public Date dueDate;
+
+        public ToDoItem(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+}
